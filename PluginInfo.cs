5637afe67f2faa7da40d6893c91f6491fc2ae2c5
Pugsluv3r <jordanpugman@gmail.com>
2025-06-30 20:52:41 -0400

    PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,7 +2,7 @@
 {
     internal class PluginInfo
     {
-        public const string GUID = "org.iidk.gorillatag.menutemplate";
+        public const string GUID = "org.pugs.gorillatag.eyerockreborn";
         public const string Name = "EyeRock Reborn";
         public const string Description = "Made By Jordan M";
         public const string Version = "1.0.0";
