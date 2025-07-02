62a26eca2b46fa80eae7cb1aa77c2c49596e163e
Joergen <Joergen@JORGEN>
2025-07-02 03:39:01 -0400

    TRUST REAL  BAN GUN ADDED

--- Mods/Overpowered.cs
+++ Mods/Overpowered.cs
@@ -6,6 +6,9 @@ namespace eyerockreborn.Mods
 {
     internal class Overpowered
     {
-
+        public static void BANGUNREAL100PERCENTNOTDETECTED()
+        {
+            //No overpowered mods this is a joke numbskull
+        }
     }
 }
